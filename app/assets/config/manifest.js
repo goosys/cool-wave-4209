8206307997a093a834d2b5e52845a816c806f187
nakamura <shop@goosys.sakura.ne.jp>
2024-11-01 15:30:13 +0000

    feat: Update asset manifest and stylesheets for administrate integration

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,4 @@
-//= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
+//= link administrate/application.css
+//= link administrate-field-nested_has_many/application.css
+//= link administrate/application.js
+//= link administrate-field-nested_has_many/application.js
