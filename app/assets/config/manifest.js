3d95e90b1c262cbb0fe6b350dc0edf61d49b137a
nakamura <shop@goosys.sakura.ne.jp>
2025-03-25 11:11:37 +0000

    feat: Refactor around asset pipeline

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,3 @@
-//= link administrate/application.css
+//= link actiontext.css
 //= link administrate-field-nested_has_many/application.css
-//= link administrate/application.js
 //= link administrate-field-nested_has_many/application.js
--- config/initializers/administrate.rb
+++ config/initializers/administrate.rb
@@ -1,7 +1,8 @@
 Rails.application.config.after_initialize do
-  Administrate::Engine.add_javascript 'administrate/administrate'
-  Administrate::Engine.add_stylesheet 'administrate/administrate'
-  # Administrate::Engine.add_stylesheet 'actiontext'
+  # manifest.jsでlinkしたcssを追加する
+  #   administrate/applicationはGem側でadd_stylesheetされているし、Gem側のmanifest.jsでlinkもされているため諸々不要
+  #   administrate-field-nested_has_manyはGem側でadd_stylesheetされているためここでは不要
+  Administrate::Engine.add_stylesheet 'actiontext' # このactiontextはapp/assets/stylesheetsのactiontext.css
 end
 
 AdministrateRansack.configure do |config|
